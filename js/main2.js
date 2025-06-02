df93c3029640e9f6cb64957e3efcaa34c78783ef
Ibrahim-Elmadbouly <ibrahim.elmadbouly@gmail.com>
2025-06-02 23:55:05 +0300

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -56,70 +56,34 @@ var quoteText = document.getElementById("quote");
 var authorName = document.getElementById("author");
 var infoText = document.getElementById("info");
 var counter =document.getElementById("counter");
+var viewedQuotes = [];
 
-// Each time the next button is clicked, a random quote is displayed, tottal number of quotes is reduced,
-// and if no quotes are left, the "reset" button is shown.
+// Each time the next button is clicked, a random quote is displayed, viewed quotes are tracked,
+// and the counter is updated. If all quotes have been viewed, the next button is hidden,
 nextBtn.addEventListener("click", function () {
     var randomNumber = getRandomQuote(quotesDB.length);
+    // Check if the random quote has already been viewed
+    do {
+        randomNumber = getRandomQuote(quotesDB.length);
+    } while (viewedQuotes.includes(randomNumber));
+    // Display the quote and author
     var quoteIndex = quotesDB[randomNumber];
     quoteText.style = "visibility: visible;"
     authorName.style = "visibility: visible;"
     quoteText.innerHTML = quoteIndex.quote;
     authorName.innerHTML = quoteIndex.author;
-    quotesDB.splice(randomNumber, 1);
-    counter.innerHTML = quotesDB.length;
-    console.log(`Number of Quotes left: ${quotesDB.length}, Random Number: ${randomNumber}`);
-    // If no quotes are left, show the reset button and hide the next button
-    if (quotesDB.length === 0) {
+    viewedQuotes.push(randomNumber);
+    counter.innerHTML = quotesDB.length - viewedQuotes.length;
+    console.log(`Number of Quotes left: ${quotesDB.length - viewedQuotes.length}, Random Number: ${randomNumber}`);
+    // If all quotes have been viewed, hide the next button and show the reset button
+    if (viewedQuotes.length === quotesDB.length) {
         nextBtn.style = "visibility: hidden;"
         resetBtn.style = "visibility: visible;"
         infoText.innerHTML = "No more quotes available today. Click 'Reset' to start over";
         infoText.style = "color : red;"
-        // Reset the value of quotesDB, hide the reset button and show the next button
+        // Reset viewed quotes and update the counter when the reset button is clicked
         resetBtn.addEventListener('click', function () {
-            quotesDB = [
-                {
-                    quote: "“Be yourself; everyone else is already taken.”",
-                    author: "― Oscar Wilde"
-                },
-                {
-                    quote: "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
-                    author: "― Marilyn Monroe"
-                },
-                {
-                    quote: "“So many books, so little time.”",
-                    author: "― Frank Zappa"
-                },
-                {
-                    quote: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
-                    author: "― Albert Einstein"
-                },
-                {
-                    quote: "“A room without books is like a body without a soul.”",
-                    author: "― Marcus Tullius Cicero"
-                },
-
-                {
-                    quote: "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
-                    author: "― Bernard M. Baruch"
-                },
-                {
-                    quote: "“You only live once, but if you do it right, once is enough.”",
-                    author: "― Mae West"
-                },
-                {
-                    quote: "“Be the change that you wish to see in the world.”",
-                    author: "― Mahatma Gandhi"
-                },
-                {
-                    quote: "“In three words I can sum up everything I've learned about life: it goes on.”",
-                    author: "― Robert Frost"
-                },
-                {
-                    quote: "“If you tell the truth, you don't have to remember anything.”",
-                    author: "― Mark Twain"
-                }
-            ];
+            viewedQuotes = [];
             nextBtn.style = "visibility: visible;";
             resetBtn.style = "visibility: hidden;";
             infoText.innerHTML = "Press the button below to receive a random quote!";
